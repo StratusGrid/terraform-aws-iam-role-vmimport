ae190929612d8ca97cd1bd7b8b239143e34095b0
JoseMotto-sg <jose.motto@stratusgrid.com>
2022-10-19 15:17:10 +0200

    module refactor

--- versions.tf
+++ versions.tf
@@ -1,7 +1,10 @@
 terraform {
+  required_version = "~> 1.1"
+
   required_providers {
     aws = {
-      version = ">= 3.63"
+      source  = "hashicorp/aws"
+      version = ">= 4.9"
     }
   }
 }
